8d92aac11f0c558dac00d457dac38afa073b5837
ariana124 <ariana.bibiano@gmail.com>
2020-08-14 11:13:52 -0700

    created reference to static files

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const request = require("request");
 
 const app = express();
 
+// The express.static grabs uses all the static files within the folder public.
+app.use(express.static("public"));
+
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
+});
+
+// When we request the home route from our server we deliver the signup page.
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
 });
