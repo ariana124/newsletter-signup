53b56b63584283b17ca11e78c62f61cf1867bce2
ariana124 <ariana.bibiano@gmail.com>
2020-08-14 11:37:10 -0700

    created post method

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 // The express.static grabs uses all the static files within the folder public.
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
 });
@@ -14,4 +16,14 @@ app.listen(3000, function() {
 // When we request the home route from our server we deliver the signup page.
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let email = req.body.email;
+
+    console.log(firstName, lastName, email);
+
 });
