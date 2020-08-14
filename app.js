2ed1c1aa0c2cc0d061a9e57cab2683836608c6d4
ariana124 <ariana.bibiano@gmail.com>
2020-08-14 16:54:06 -0700

    added env variable for heroku

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server is running on port 3000.");
 });
 
@@ -60,7 +60,7 @@ app.post("/", function(req, res) {
         })
     });
 
-    //request.write(jsonData);
+    request.write(jsonData);
     request.end();
 
 });
